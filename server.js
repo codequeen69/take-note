95d36edd9d456f3fec95bc625ecc7bf940a57322
Venus DM <crhowe90@gmail.com>
2022-08-07 16:29:42 -0500

    createNewNote function

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const { notes } = require('./db/db.json')
+const uniqid = require('uniqid'); 
+const  notes = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,41 +15,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// function createNewNote(){
+function createNewNote(){
 
-// }
-// const addNewNote = newNote => {
-//     //read db to get data saved
-//     fs.readFile('/db/db.json', 'utf8', (err, data) =>{
+}
 
-//     })
-//     fs.writeFileSync(
-//         path.join(__dirname, './db/db.json'),
-//         JSON.stringify({})
-//     )
-// }
 
 app.get('/api/notes', (req, res) =>{
-    let results = notes
-    res.json(results);
+    let results = notes;
     console.log(results)
+    res.json(results);
 })
 
 //will receive a new note to save on the req body, add to db.json
 //file then return the new note to the client. Give each note unique id with an npm package.
 app.post('/api/notes', (req, res)=> {
-    console.info(`${req.method} request received to add a note`)
-    const {title, text} = req.body
-    const newNote = {
+
+
+   // if any data in req.body is incorrect, send 400 error back
+   if (!validateNote(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+} else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+}
+});
+
+
+
+const createNewNote = newNote => {
+    let newNote = {
         title: req.body.title,
         text: req.body.description,
-        id: uuidv4()
-    }
-    console.log(newNote);
-    console.log(newNote.id);
- 
-    //get existing notes from db file
-    fs.readFile('./db/db.json', 'utf8', (err, data) =>{
+        id: uniqid()
+    };
+    //read db to get data saved
+    fs.readFile('/db/db.json', 'utf8', (err, data) =>{
         if (err){
             throw err;
         }else{
@@ -59,22 +59,18 @@ app.post('/api/notes', (req, res)=> {
             //add new note
             parsedNotes.push(newNote);
 
-            //write new note back to db file
-            fs.writeFile(
-                './db/db.json',
-                JSON.stringify(parsedNotes, null, 4),
-                (err)=>{
-                    if(err){
-                        throw err;
-                    }else console.info('Successfully added note!');
-                    
-                }
-            )
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(parsedNotes, null, 4),
+        (err)=>{
+            if(err){
+                throw err;
+            }else console.info('Successfully added note!');
+            
         }
-    })
-})
-
-
+    );
+    return newNote;
+}
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
