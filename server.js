40c11d920fa09c2b89193829b5a5d0f279fe908b
Venus DM <crhowe90@gmail.com>
2022-08-08 17:48:55 -0500

    delete function working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const notes = require('./db/db.json')
-const uniqid = require('uniqid'); 
+const uniqid = require('uniqid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 //can't fetch the body or different properties of fetch request without this
 app.use(express.json());
 
-app.get('/api/notes', (req, res) =>{
+app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(results)
     res.json(results);
@@ -22,13 +22,10 @@ app.get('/api/notes', (req, res) =>{
 
 //will receive a new note to save on the req body, add to db.json
 //file then return the new note to the client. Give each note unique id with an npm package.
-app.post('/api/notes', (req, res)=> {
-
-   // if any data in req.body is incorrect, send 400 error back
-//    //
+app.post('/api/notes', (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
-//}
+   
 });
 
 
@@ -38,48 +35,37 @@ function createNewNote(body, notesArray) {
         text: body.text,
         id: uniqid()
     };
-    // //read db to get data saved
-    // fs.readFile('/db/db.json', 'utf8', (err, data) =>{
-    //     if (err){
-    //     console.error(err);
-    //     return;
-    //     }else{
-    //         //convert string to JSON object
-    //         const parsedNotes = JSON.parse(data);
-
-    //         //add new note
-    //         parsedNotes.push(newNote);
-
-   notesArray.push(newNote);
+    notesArray.push(newNote);
+    console.log(notesArray);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 4),
+        JSON.stringify(notesArray, null, 2),
     );
     return newNote;
 };
 
-function validateNote(note){
-    if (!note.title || typeof note.title !== 'string'){
-        return false;
-    }if (!note.text || typeof note.text !=='string'){
-        return false;
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id === id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 4),
+            );
+            break;
+        }
     }
 }
 
 //delete a note by id
-app.delete('/api/notes/:id', function (req, res){
- notes.splice(req.params.id, 1);
- fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 4),
-    );
- 
+app.delete('/api/notes/:id', function (req, res) {
+    deleteNote(req.params.id, notes);
+    res.json(true);
 });
 
-function findById(id, notesArray){
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-    }
+
 // function deleteNote(id, notesArray) {
 //     for (var i=0; i<notesArray.length; i++) {
 //         let note = notesArray[i];
@@ -98,13 +84,13 @@ app.get('/', (req, res) => {
 })
 
 //will return the  notes.html
-app.get('/notes', (req, res) =>{
-    res.sendFile(path.join(__dirname,'./public/notes.html' ))
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 
 //wildcard! Will be directed to the homepage if a path doesn't work out. Must come last!
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '.public/index.html'))
 })
 
