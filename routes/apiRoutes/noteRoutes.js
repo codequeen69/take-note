f1dcf880ecdf047ed4e60e4e6b76bf34bb79646f
Venus DM <crhowe90@gmail.com>
2022-08-09 15:12:27 -0500

    moved note functions to lib

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
 const path = require('path');
+const {createNewNote, deleteNote} =require('../../lib/notes');
 const notes = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    console.log(results)
+    res.json(results);
+})
+
+//will receive a new note to save on the req body, add to db.json
+//file then return the new note to the client. Give each note unique id with an npm package.
+router.post('/notes', (req, res) => {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+   
+});
+
+//delete a note by id
+router.delete('/notes/:id', function (req, res) {
+    deleteNote(req.params.id, notes);
+    res.json(true);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,80 +8,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 //if a request is given to this file give them the other files in the folder
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true }));
 
 //can't fetch the body or different properties of fetch request without this
 app.use(express.json());
 
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(results)
-    res.json(results);
-})
-
-//will receive a new note to save on the req body, add to db.json
-//file then return the new note to the client. Give each note unique id with an npm package.
-app.post('/api/notes', (req, res) => {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-   
-});
-
-
-function createNewNote(body, notesArray) {
-    let newNote = {
-        title: body.title,
-        text: body.text,
-        id: uniqid()
-    };
-    notesArray.push(newNote);
-    console.log(notesArray);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2),
-    );
-    return newNote;
-};
-
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id === id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 4),
-            );
-            break;
-        }
-    }
-}
-
-//delete a note by id
-app.delete('/api/notes/:id', function (req, res) {
-    deleteNote(req.params.id, notes);
-    res.json(true);
-});
-
-
-// function deleteNote(id, notesArray) {
-//     for (var i=0; i<notesArray.length; i++) {
-//         let note = notesArray[i];
-//         if(note[i].id == id){
-//           notesArray.splice(i, 1);
-//           fs.writeFileSync(
-//             path.join(__dirname, './db/db.json'),
-//             JSON.stringify(notesArray, null, 2),
-//         );
-//         }
-//     }
-
-// }
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
+
 
 //will return the  notes.html
 app.get('/notes', (req, res) => {
